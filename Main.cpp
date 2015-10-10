dfd49aa1fbf7a10a0315db2eb3c159d587f97c23
Nicolas Pichot <pichot.n@gmail.com>
2015-10-10 17:47:40 +0200

    Structure programme Création de la fenêtre graphique et mise en place de la boucle du programme

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,26 @@
-#include <iostream>
+#include "Main.h"
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello world" << endl;
-	system("pause");
+	// Chargement de la fenêtre
+	RenderWindow window(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, 32),"Snake");
+
+	//Lancement de la boucle principale
+	while (window.isOpen())
+	{
+		//Controle des inputs claviers 
+
+		//Mise à jour du serpent 
+
+		//Gestion des actions
+
+		//Création de la map 
+
+		//Affichage
+		window.display();
+	}
+	
 	return 0;
 }
---
+++ Main.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <stdio.h>
+#include <iostream>
+#include <SFML/Graphics.hpp> //Chargement bibliothèque Graphique
+
+using namespace std;
+using namespace sf; // Pour utiliser la bibliothèque graphique plus rapidement.
+
+					// Définition de la fenêtre
+const unsigned int WINDOW_WIDTH = 800;
+const unsigned int WINDOW_HEIGHT = 600;
