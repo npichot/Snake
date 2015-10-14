432c1c6c69064f1b5e0b82fc031a4235297f4819
Nicolas Pichot <pichot.n@gmail.com>
2015-10-14 22:53:41 +0200

    Modification de structure de Serpent
    Evite les problèmes de compilations en définissant les éléments dans le
    bon ordre

--- Serpent.h
+++ Serpent.h
@@ -1,6 +1,23 @@
 #pragma once
 using namespace std;
 
+typedef enum  //Pour la tête du serpent
+{
+	NORTH,
+	SOUTH,
+	EAST,
+	WEST,
+}Orientation;
+
+struct Head { //Structure pour la tête du serpent
+	int line, column;//Position ligne/colonne dans la grille de jeu
+	Orientation orientation; //orientation
+};
+
+struct Tail { // Structure pour identifier la queue du serpent
+	int row, column;
+};
+
 //Classe permettant de gérer le serpent en temps réel.
 class Serpent
 {
@@ -12,22 +29,9 @@ private:
 	Tail tail;
 };
 
-enum Orientation //Pour la tête du serpent
-{
-	NORTH,
-	SOUTH,
-	EAST,
-	WEST,
-};
 
-struct Head { //Structure pour la tête du serpent
-	int line, column;//Position ligne/colonne dans la grille de jeu
-	Orientation orientation; //orientation
-};
 
-struct Tail { // Structure pour identifier la queue du serpent
-	int row, column;
-};
+
 
 
 
