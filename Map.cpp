2ac4ded58032a6d11fd38ec644714955a4f9a566
Nicolas Pichot <pichot.n@gmail.com>
2015-10-14 23:14:39 +0200

    Correction initalisation de la map
    Problème d'initialisation du vector 2 dimensions field

--- Map.cpp
+++ Map.cpp
@@ -5,16 +5,20 @@
 // La map prend la forme d'une matrice où chaque élément symbolise un élément du décor
 Map::Map()
 {
-	int row, column;//Paramètre de la map, nombre 
+	int row_number, column_number;//Paramètre de la map, nombre 
 
 	//Calcule des paramètres 
-	column = floor((WINDOW_WIDTH - 3 * TILE_SIZE) / TILE_SIZE);//On prévoit une marge min de 3 tile (~100 px)
-	row= floor((WINDOW_HEIGHT - 3 * TILE_SIZE) / TILE_SIZE);
+	column_number = floor((WINDOW_WIDTH - 3 * TILE_SIZE) / TILE_SIZE);//On prévoit une marge min de 3 tile (~100 px)
+	row_number= floor((WINDOW_HEIGHT - 3 * TILE_SIZE) / TILE_SIZE);
 	
 	//Remplissage de la map
-	for (int i = 0; i < row; ++i)
-		for (int j = 0; j < column; ++j)
-			field[i].push_back(EMPTY);
+	for (int i = 0; i < row_number; ++i)
+	{
+		vector<Tiles> row;
+		for (int j = 0; j < column_number; ++j)
+			row.push_back(EMPTY);
+		field.push_back(row);
+	}
 }
 
 Map::~Map()
