106059482d52b3e6ce56efa3b10bd85457f1b57f
Nicolas Pichot <pichot.n@gmail.com>
2015-10-17 17:22:08 +0200

    Suppression test map

--- Main.cpp
+++ Main.cpp
@@ -6,13 +6,7 @@ int main()
 {
 	// Chargement de la fenêtre
 	RenderWindow window(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, 32),"Snake");
-	Map map;
-
-	//Error
-	map.updateField(-1, 2, HEAD_WEST);
-	map.updateField(100, 3, BODY_WEST);
-	map.updateField(10, -5, BODY_WEST);
-	map.updateField(11, 40, BODY_NORTH);
+	Map map("MapConfig/Config1.dat");
 
 	//Lancement de la boucle principale
 	while (window.isOpen())
@@ -22,14 +16,6 @@ int main()
 		//Mise à jour du serpent 
 
 		//Gestion des actions
-
-		//Test de l'affichage
-		map.updateField(0, 0, BUSHES);
-		map.updateField(3, 6, FRUIT);
-		map.updateField(10, 2, HEAD_WEST);
-		map.updateField(10, 3, BODY_WEST);
-		map.updateField(10, 4, BODY_WEST);
-		map.updateField(11, 4, BODY_NORTH);
 
 		//dessin de la map 
 		window.clear();
