fcc4c824a2096ec218b851d4a6dd45cf783f1383
Ouss7 <oussama.taghi@student.ecp.fr>
2015-10-21 11:01:23 +0200

    Ajout d'une classe head
    (Ne pas oubleir d'inclureles fichiers dans visual studio)

---
+++ Head.cpp
@@ -0,0 +1,32 @@
+#include "Head.h"
+
+using namespace std;
+
+//TODO définition de la classe
+
+Head::Head()   // Valeurs initiales à revoir
+{
+	m_line = 10;
+	m_column = 10;
+	m_orientation = EAST; // Utilisation de l'enum à revoir
+}
+
+Head::Head(int line, int column, Orientation orientation) : m_line(line), m_column(column), m_orientation(orientation)
+{
+
+}
+
+int Head::getLine() const
+{
+	return m_line;
+}
+
+int Head::getColumn() const
+{
+	return m_column;
+}
+
+Orientation Head::getOrientation()
+{
+	return m_orientation;
+}
---
+++ Head.h
@@ -0,0 +1,26 @@
+#pragma once
+
+
+typedef enum  //Pour la tête du serpent
+{
+	NORTH,
+	SOUTH,
+	EAST,
+	WEST,
+}Orientation;
+
+class Head { //Classe pour la tête du serpent
+
+	//Méthodes
+	Head();
+	~Head();
+	Head(int line, int column, Orientation orientation);
+	int getLine() const;
+	int getColumn() const;
+	Orientation getOrientation();
+	
+
+	//Attributs
+	int m_line, m_column;//Position ligne/colonne dans la grille de jeu
+	Orientation m_orientation; //orientation
+};
