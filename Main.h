a3d67167c84c719216659630edbc862bc84286fa
npichot <pichot.n@gmail.com>
2015-11-28 09:40:33 +0100

    Update Main.h
    Ajout argument highscore dans la fonction play

--- Main.h
+++ Main.h
@@ -11,6 +11,6 @@
 const unsigned int WINDOW_WIDTH = 1280;
 const unsigned int WINDOW_HEIGHT = 720;
 
-void play(sf::RenderWindow & window);
+void play(sf::RenderWindow & window, int highscore);
 
 #endif
