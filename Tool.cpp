838b6ab86134054aba0b8f59e3fb1dccb10f2487
Nicolas Pichot <none@none>
2015-12-01 16:08:42 +0100

    Clean de la classe Tool et ajout des commentaires

--- Tool.cpp
+++ Tool.cpp
@@ -2,17 +2,16 @@
 using namespace std;
 using namespace sf;
 
-
+/*
+Constructeure de la classe qui initialise les propriétés des rectangles.
+Cette classe est une classe abstraite
+*/
 Tool::Tool(Texture t, string s)
 	: name(s)
 {
-	
 	setTexture(new Texture(t));
-
 	setOutlineColor(Color::Red);
-
 	setSize(Vector2f(TILE_SIZE, TILE_SIZE));
-
 }
 
 Tool::~Tool()
